b681d74ccb987e2089e76c57f037c918849d4c12
IDGS-805-21002222 <utlcuitlahuac@gmail.com>
2025-01-16 22:51:47 -0600

    Día 16 de enero, archivos

--- practica_16Enero.py
+++ practica_16Enero.py
@@ -3,9 +3,6 @@
 # 
 #  
 
-
-import math
-
 x1 = float(input("Ingresa la coordenada x del primer punto: "))
 y1 = float(input("Ingresa la coordenada y del primer punto: "))
 
@@ -13,6 +10,6 @@ x2 = float(input("Ingresa la coordenada x del segundo punto: "))
 y2 = float(input("Ingresa la coordenada y del segundo punto: "))
 
 
-distancia = math.sqrt((x2 -x1)**2 + (y2 -y1)**2)
+distancia = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 
-print(f"La distancia entre los puntos ({x1}, {y1} y {x2}, {y2} es de: {distancia:.2f})")
+print(f"La distancia entre los puntos ({x1}, {y1}) y ({x2}, {y2}) es de: {distancia:.2f}")
